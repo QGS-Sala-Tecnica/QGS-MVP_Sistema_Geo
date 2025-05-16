32cb6dd4d6d56c0b066cf0cb9a78f0a28ee2c22d
QGS-Sala-Tecnica <qgssalatecnica@gmail.com>
2025-05-16 10:48:22 -0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.set_page_config(layout="wide")
+st.title("QGS-MVP_Sistema_Geo")
+st.markdown("**Projeto piloto: Nova Conquista**")
+st.success("MVP iniciado com sucesso.")
